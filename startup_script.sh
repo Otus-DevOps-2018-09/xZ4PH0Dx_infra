477fdb427db34582486d83420c922f5034c994e3
Ruslan Khalimov <dreamfall3r@gmail.com>
2018-06-25 00:58:21 +0200

    deleted redundant files

--- startup_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/appuser
-git clone -b cloud-testapp https://github.com/Otus-DevOps-2018-05/xZ4PH0Dx_infra.git scripts
-sudo ./scripts/install_mongod.sh &&
-sudo ./scripts/install_ruby.sh &&
-sudo ./scripts/deploy.sh
-rm -f scripts
-
